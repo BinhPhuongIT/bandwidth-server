af9b863b836907bb8aaa134c1af2518b16d8046f
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-04-04 15:53:15 +0700

    update final

--- install-iperf3-server.sh
+++ install-iperf3-server.sh
@@ -1,1 +1,25 @@
+# Install iperf3
 apt install -y iperf3
+
+# Create service bandwidth
+cat << EOF > /etc/systemd/system/bandwidth.service
+[Unit]
+Description=Start iperf server
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/usr/bin/iperf3 -s
+Restart=on-failure
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+# Start service
+systemctl enable bandwidth.service
+systemctl start bandwidth.service
+
+echo "Done"
